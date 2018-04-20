5cf3a0bc18e254b582552ab2086f189f854c884c
Brandon Bruce <bran8219@fedora-workstation.rackspace.corp>
2018-04-20 14:39:49 -0500

    Hello World practice script

---
+++ hello.py
@@ -0,0 +1,15 @@
+#This is a program to ask for a name and then say issue a greeting.
+
+print('Hello world!')
+print('What is your name?')
+
+myName = input()
+
+print('It is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+print('What is your age?')
+
+myAge = input()
+
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
