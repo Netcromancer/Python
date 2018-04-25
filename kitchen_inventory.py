3a204d679591069bfefb0d9946fbf6e234c26e93
Brandon Bruce <bran8219@fedora-workstation.rackspace.corp>
2018-04-25 09:55:13 -0500

    Cleaned up for master. Removed API Key.

--- kitchen_inventory.py
+++ kitchen_inventory.py
@@ -9,9 +9,9 @@ import urllib.error
 import json
 
 #Variables
-api_key = '219860C974FF6AB2CDE9B1E152807F04'
+#Need to register at https://api.upcdatabase.org/product and obtain API Key.
+api_key = <API_KEY>
 address = 'https://api.upcdatabase.org/product/'
-
 #Function to collect and convert UPC codes into items.
 def upc():
 	print ("Enter item")
@@ -22,16 +22,40 @@ def url(item):
 	ready_item = item + '/'
 	combined = address + ready_item + api_key
 	return combined
+#Commented out for testing. This works, but error handling is not working properly.
+#def request(combined):
+#	opener = urllib.request.build_opener()
+#	f = opener.open(combined)
+#	json = json.loads(f.read())
+#	try: urllib.request.urlopen(opener)
+#	except urllib.error.URLError as e:
+#		print(e.reason)
+#Print the entire JSON. This is for debugging and will be removed later.
+#	print(json)
+#This is the intended output and should remain.
+#	print(json['title'])
 
 def request(combined):
 	opener = urllib.request.build_opener()
 	f = opener.open(combined)
+	try: urllib.request.urlopen(opener)
+	except urllib.error.URLError as e:
+		print(e.reason)
 	json = json.loads(f.read())
+#Print the entire JSON. This is for debugging and will be removed later.
 	print(json)
+#This is the intended output and should remain.
 	print(json['title'])
 
 
-request(url(upc()))
+item = upc()
+combined = url(item)
+request(combined)
+
+
+
+
+#request(url(upc()))
 
 #output = requests.get(combined)
 #print(output)
