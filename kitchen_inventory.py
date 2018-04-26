803702d7dd594bbcf587325653b9b1e68b794b03
Brandon Bruce <bran8219@fedora-workstation.rackspace.corp>
2018-04-26 13:04:40 -0500

    Added while loop. Working on how to create object, dictionary.

--- kitchen_inventory.py
+++ kitchen_inventory.py
@@ -10,8 +10,8 @@ import json
 
 #Variables
 #Need to register at https://api.upcdatabase.org and obtain API Key.
-api_key = <API_KEY>
-address = 'https://api.upcdatabase.org/product'
+api_key = '903CA92CA0FEC646E9DA686A03568799'
+address = 'https://api.upcdatabase.org/product/'
 #Function to collect and convert UPC codes into items.
 def upc():
 	print ("Enter item")
@@ -27,19 +27,33 @@ def request(combined):
 	opener = urllib.request.build_opener()
 	f = opener.open(combined)
 	json1 = json.loads(f.read())
-	try: urllib.request.urlopen(opener)
-	except urllib.error.URLError as e:
-		print(e.reason)
+
+#Attempting to add error handling. Not working.
+#	try: urllib.request.urlopen(opener)
+#	except urllib.error.URLError as e:
+#		print(e.reason)
+
+
 #Print the entire JSON. This is for debugging and will be removed later.
 	print(json1)
 #This is the intended output and should remain.
-	print(json1['title'])
+	title = print(json1['title'])
+	print(json1['description'])
 
-
+#This collection of functions works.
+#Commented to test loops below.
 item = upc()
 combined = url(item)
 request(combined)
 
+while True:
+	try:
+		item = int(upc())
+	except ValueError:
+		print('Exiting')
+		break
+	combined = url(item)
+	request(combined)
 
 
 
